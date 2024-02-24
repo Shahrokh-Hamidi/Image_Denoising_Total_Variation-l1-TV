b415f8353601115b185e6662789c8aa0604e2aca
Shahrokh Hamidi <156338354+Shahrokh-Hamidi@users.noreply.github.com>
2024-02-24 18:12:38 -0500

    Update img_denoising_TV.py

--- src/img_denoising_TV.py
+++ src/img_denoising_TV.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     os.getcwd()
     os.chdir("../image")
-    path = os.path.join('AE_noisy.png')
+    path = os.path.join('noisy_image.png')
     
     img = readImage.read_image(path)
     
